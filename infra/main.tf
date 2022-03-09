33f6e1c9292bc861f766abc5167996446ffe3da4
Saurav <sauravads123@gmail.com>
2022-03-09 13:24:04 +0545

    feat : create a bucket and upload static content from these

--- infra/main.tf
+++ infra/main.tf
@@ -4,6 +4,18 @@ resource "google_app_engine_application" "app" {
   location_id = var.app_engine_region
 }
 
+resource "google_storage_bucket" "static_content_bucket" {
+  name                        = "${var.project_id}-appengine-static-content"
+  location                    = "ASIA-SOUTH1"
+  force_destroy               = true
+  uniform_bucket_level_access = true
+}
+
+resource "google_storage_bucket_object" "default_node_server" {
+  name   = "default_service_node.zip"
+  bucket = google_storage_bucket.static_content_bucket.name
+  source = "../default_service_node.zip"
+}
 resource "google_app_engine_standard_app_version" "default_service" {
   version_id = "v1"
   service    = "default"
@@ -13,18 +25,43 @@ resource "google_app_engine_standard_app_version" "default_service" {
   }
   deployment {
     zip {
-      source_url = "https://storage.googleapis.com/${var.default_service_source}/default_service.zip"
+      source_url = "https://storage.googleapis.com/${google_storage_bucket.static_content_bucket.name}/${google_storage_bucket_object.default_node_server.name}"
     }
   }
- automatic_scaling {
+  automatic_scaling {
     min_idle_instances = 0
     max_idle_instances = 1
-    
+
     standard_scheduler_settings {
       min_instances = 0
       max_instances = 1
     }
   }
-  instance_class="F1"
+  instance_class  = "F1"
   noop_on_destroy = true
+}
+
+resource "google_app_engine_standard_app_version" "front_service" {
+  version_id = "v1"
+  service    = "front"
+  runtime    = "nodejs14"
+  entrypoint {
+    shell = "npm start"
+  }
+  deployment {
+    zip {
+      source_url = "https://storage.googleapis.com/${google_storage_bucket.static_content_bucket.name}/${google_storage_bucket_object.default_node_server.name}"
+    }
+  }
+  automatic_scaling {
+    min_idle_instances = 0
+    max_idle_instances = 1
+
+    standard_scheduler_settings {
+      min_instances = 0
+      max_instances = 1
+    }
+  }
+  instance_class            = "F1"
+  delete_service_on_destroy = true
 }
